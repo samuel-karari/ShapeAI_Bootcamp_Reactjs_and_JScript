97aebf81ac9bc657fc1a2aee9b05c6e467685233
samuel-karari <85393727+samuel-karari@users.noreply.github.com>
2021-06-05 13:51:02 +0300

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+       Shape AI, a reseach and educational startup, has utilized exceptional skills of expert tutors such as Shaurya Sinha.
+       A Data Analyst Intern  at JIO to conduct an online 7 day Free Bootcamp on Web development with JavaScript, React.js, and Html.
+       These are core skills to learn especially in the programming sector proven to be essential among vairegated Data Analysis organizations.
+      </p> 
+    </div>
+  );
+}
+
+export default Note;
